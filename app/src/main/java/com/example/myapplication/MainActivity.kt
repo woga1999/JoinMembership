6a0b1858d74d9862bfc3ef8f76f3ea322cdb5ce4
woga1999 <woga1999@gmail.com>
2020-09-25 21:05:16 +0900

    Add input Password

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,9 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var inputDataArray: Array<EditText>
     private lateinit var textInputLayoutArray: Array<TextInputLayout>
-    private var isCorrectArray: Array<Boolean> = arrayOf(false,false,false,false,false,false,false)
+    private lateinit var isCorrectArray: Array<Boolean>
     private var isDone = false
     internal val viewDisposables = CompositeDisposable() // 메모리 누수를 막기 위한 클래스
+    private lateinit var errorMsgArray:Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     private fun init() {
         inputDataArray = arrayOf(editEmail, editPwd, editCheckPwd, editName)
         textInputLayoutArray = arrayOf(editEmailLayout, editPwdLayout, editCheckPwdLayout, editNameLayout)
+        isCorrectArray  = arrayOf(false,false,false,false,false,false,false)
+        errorMsgArray  = arrayOf(Constant.emailErrorMessage,  Constant.pwdErrorMessage, Constant.checkPwdErrorMessage)
 
     }
 
@@ -36,19 +39,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun inputData(){
         val inputEmail = RxTextView.textChanges(inputDataArray[0])
-            .map { Pattern.matches(Constant.regexEmail, inputDataArray[0].text.toString())}
-            .subscribe({
+            .map { it -> it.isEmpty() ||Pattern.matches(Constant.regexEmail, inputDataArray[0].text.toString())}
+            .subscribe {
                 isCorrectArray[0] = it
                 displayMsg(0,it)
-            }){
-                //Error Block
+            }
+        val inputPwd = RxTextView.textChanges(inputDataArray[1])
+            .map { it -> it.isEmpty() ||Pattern.matches(Constant.regexPwd, inputDataArray[1].text.toString()) }
+            .subscribe{
+                isCorrectArray[1] = it
+                displayMsg(1,it)
+            }
+        val checkInputPwd = RxTextView.textChanges(inputDataArray[2])
+            .map { it -> it.isEmpty() || inputDataArray[1].text.toString() == inputDataArray[2].text.toString() }
+            .subscribe{
+                isCorrectArray[2] = it
+                displayMsg(2,it)
             }
     }
 
     private fun displayMsg(index:Int, check:Boolean){
         textInputLayoutArray[index].isErrorEnabled = !check
         if(!check){
-            textInputLayoutArray[index].error = Constant.emailErrorMessage
+            textInputLayoutArray[index].error = errorMsgArray[index]
         }
     }
 
