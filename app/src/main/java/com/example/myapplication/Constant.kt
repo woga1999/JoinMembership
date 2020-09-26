3b86d7b9848843475767a0cae2ca58e8fd2fcd1c
woga1999 <woga1999@gmail.com>
2020-09-27 02:11:33 +0900

    Add progress bar when click JOIN button

--- app/src/main/java/com/example/myapplication/Constant.kt
+++ app/src/main/java/com/example/myapplication/Constant.kt
@@ -7,11 +7,11 @@ class Constant {
         val pwdErrorMessage = "대문자 1개 이상, 소문자 1개 이상, 숫자 1개 이상 포함해서 8자 이상 입력해주세요."
         val checkPwdErrorMessage = "입력하신 비밀번호와 맞지않습니다."
         val nickNameErrorMessage = "닉네임은 8자 이상 30자 이하로 입력해주세요."
-        val birthErrorMessage = "생년월일을 입력해주세요"
+        val birthErrorMessage = "생년월일을 yyyy-mm-dd 형식으로 넣어주세요"
 
         //정규식
         val regexEmail = "[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}"
         val regexPwd = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$"
-
+        val regexBirth = "[0-9]{4}-[0-9]{2}-[0-9]{2}"
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication
 
 import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.design.widget.TextInputLayout
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.MotionEvent
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.jakewharton.rxbinding2.widget.RxCompoundButton
@@ -30,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         init()
         inputData()
+        setJoin()
     }
     private fun init() {
         inputDataArray = arrayOf(editEmail, editPwd, editCheckPwd, editName, editBirth)
@@ -47,6 +53,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         btnJoin.isEnabled = true
+        loading(btnJoin)
+    }
+
+    private fun loading(btn:Button){
+        btn.setOnClickListener { v->
+            DialogTask(this).execute()
+        }
     }
 
     private fun birthTouchListener():Boolean{
@@ -57,7 +70,13 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-        return editBirth.text.toString().isNotEmpty()
+        editBirth.setOnKeyListener { v, keyCode, event ->
+            if(event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER){
+                displayDatePickerDialog(editBirth)
+            }
+            true
+        }
+        return Pattern.matches(Constant.regexBirth, editBirth.text.toString())
     }
 
     private fun displayDatePickerDialog(eT : EditText){
@@ -83,38 +102,43 @@ class MainActivity : AppCompatActivity() {
         val inputEmail = RxTextView.textChanges(inputDataArray[0])
             .map { it.isEmpty() || Pattern.matches(Constant.regexEmail, inputDataArray[0].text.toString()) }
             .subscribe {
-                isCorrectArray[0] = it
+                if(!inputDataArray[0].text.isEmpty()) isCorrectArray[0] = it
+                else isCorrectArray[0] = false
                 displayMsg(0, it)
                 setJoin()
             }
         val inputPwd = RxTextView.textChanges(inputDataArray[1])
             .map { it.isEmpty() || Pattern.matches(Constant.regexPwd, inputDataArray[1].text.toString()) }
             .subscribe {
-                isCorrectArray[1] = it
+                if(!inputDataArray[1].text.isEmpty()) isCorrectArray[0] = it
+                else isCorrectArray[1] = false
                 displayMsg(1, it)
                 setJoin()
             }
         val checkInputPwd = RxTextView.textChanges(inputDataArray[2])
             .map { it.isEmpty() || inputDataArray[1].text.toString() == inputDataArray[2].text.toString() }
             .subscribe {
-                isCorrectArray[2] = it
+                if(!inputDataArray[2].text.isEmpty()) isCorrectArray[0] = it
+                else isCorrectArray[2] = false
                 displayMsg(2, it)
                 setJoin()
             }
         val inputNickName = RxTextView.textChanges(inputDataArray[3])
             .map { it.isEmpty() || it.length in 8..30 }
             .subscribe {
-                isCorrectArray[3] = it
+                if(!inputDataArray[3].text.isEmpty()) isCorrectArray[0] = it
+                else isCorrectArray[3] = false
                 displayMsg(3, it)
                 setJoin()
             }
         val setBirthDate = RxTextView.textChanges(inputDataArray[4])
-            .map { it.isEmpty() || birthTouchListener()}
+            .map { birthTouchListener() || Pattern.matches(Constant.regexBirth, inputDataArray[4].text.toString())}
             .subscribe {
                 isCorrectArray[4] = it
-                displayMsg(4, it)
+                if(!inputDataArray[4].text.isEmpty()) displayMsg(4, it)
                 setJoin()
             }
+
         val choiceSexRadioGroup = RxRadioGroup.checkedChanges(sexRadioGroup)
             .subscribe{
                 //val radio: RadioButton = findViewById(it)
@@ -140,6 +164,28 @@ class MainActivity : AppCompatActivity() {
         if(!check){
             textInputLayoutArray[index].error = errorMsgArray[index]
         }
+    }
+
+    class DialogTask(var activity: MainActivity) : AsyncTask<Void, Void, Void>(){
+
+        var dialog = Dialog(activity, android.R.style.Theme_Translucent_NoTitleBar)
+        override fun onPreExecute() {
+            val view = activity.layoutInflater.inflate(R.layout.progress_bar,null)
+            dialog.setContentView(view)
+            dialog.setCancelable(false)
+            dialog.show()
+            super.onPreExecute()
+        }
+        override fun doInBackground(vararg params: Void?): Void? {
+            Thread.sleep(2000)
+            return null
+        }
+
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+            dialog.dismiss()
+        }
+
     }
 
     override fun onStop() {
